cc2d9f1ad9003701bbc16015672b801471ca43a8
zetcan333 <98547216+zetcan333@users.noreply.github.com>
2023-12-16 16:06:52 +0300

    Create script-New.sql

---
+++ DataBase/part_2_SQLscripts/work_with_car/script-New.sql
@@ -0,0 +1,2 @@
+INSERT INTO public."Cars" (car_id, model, car_year, color, reg_number, status, price_per_day) 
+VALUES ('5'::integer, 'toyota supra'::text, '2020'::integer, 'Красный'::character varying, 'Н010НХ'::character varying, 'Свободна'::character varying, '7000'::numeric)
