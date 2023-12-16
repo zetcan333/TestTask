a5822631e0b2c6ce7aadaf29f60b6d249bc4fefd
zetcan333 <98547216+zetcan333@users.noreply.github.com>
2023-12-16 16:10:42 +0300

    Update script-New.sql

--- DataBase/part_2_SQLscripts/work_with_client/script-New.sql
+++ DataBase/part_2_SQLscripts/work_with_client/script-New.sql
@@ -1,2 +1,2 @@
 INSERT INTO public."Clients" (client_id, client_name, phone, license, is_blocked) 
-VALUES ('6'::integer, 'Петров Евгений'::character varying, '89004002287'::character varying, '897554'::character varying, false::boolean)
+VALUES ('6'::integer, 'Петров Евгений'::character varying, '89004002287'::character varying, '897554'::character varying, false::boolean);
